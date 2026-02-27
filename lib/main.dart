409b94c0eb0bdea3b0d4b1856e5dd8e847d5ea03
Umega Eshan <umegaeshan@gmail.com>
2026-02-27 22:25:27 +0530

    Replace Container with Column-based UI
     vertical Column layout: two centered Text widgets with distinct background colors and a large Expanded FlutterDash icon. Also disable the debug banner (debugShowCheckedModeBanner: false) and add an import for cupertino.dart. This refactors the main UI to present multiple lines of content and a prominent icon instead of the previous fixed-size container.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,20 +11,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Container",
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-          body: Container(
-            width: 60,
-            height: 60,
-            margin: EdgeInsets.fromLTRB(100, 150, 200, 250),
-            padding: EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.amber,
-              border: Border.all(color: Colors.black, width: 2),
-            ),
-            child: Center(child: Text("Flutter")),
+          body: Column(
+            children: [
+              Center(
+                child: Text(
+                  "Deliver features faster",
+                  style: TextStyle(backgroundColor: Colors.amber, fontSize: 30),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Craft beautiful UIs",
+                  style: TextStyle(
+                    backgroundColor: const Color.fromARGB(255, 230, 126, 94),
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Expanded(child: Icon(Icons.flutter_dash, size: 200)),
+            ],
           ),
         ),
       ),
