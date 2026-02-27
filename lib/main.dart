bfdc45c141becf19096ee6e01ac6528930e53ce3
Umega Eshan <umegaeshan@gmail.com>
2026-02-27 15:29:40 +0530

    create simple app text,icon,style,align

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +9,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: const Text(
+              "Easy Flutter App",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.orange,
+          leading: const Icon(Icons.menu, size: 30),
+          actions: const [
+            Icon(Icons.search, size: 30),
+            Icon(Icons.shopping_bag, size: 30),
+          ],
+        ),
+        body: const Center(
+          child: Text(
+            "Hello Friends . My Name Is Flutter",
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      ),
+    );
   }
 }
